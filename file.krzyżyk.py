78c6419bd91d6b0e92a4e45ab09a683d229afef5
krystian-kowalik <kjkowalik05@gmail.com>
2022-03-12 11:12:03 +0100

    Added board printing. Refactor code into function.

--- file.krzyżyk.py
+++ file.krzyżyk.py
@@ -1,7 +1,20 @@
 import random
 
 
-first_player = random.randint(0, 1)
-name_dict = {0: 'computer',
-             1: 'user'}
-print("Begin: ", name_dict[first_player])
+def print_board():
+    for x in range(3):
+        print("  | | ")
+        if x < 2:
+            print(" - - - ")
+
+def initialize_first_player():
+    first_player = random.randint(0, 1)
+    name_dict = {0: 'computer',
+                 1: 'user'}
+    print("Begin: ", name_dict[first_player])
+
+
+initialize_first_player()
+print_board()
+
+
